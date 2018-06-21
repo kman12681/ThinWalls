e04a04c5e4359feea51ec7c0fe37802c84f62c64
maggiehubbard <maggiehubbard@wayne.edu>
2018-06-21 13:57:13 -0400

    Added logic to calculate and dsplay scores on details view

--- ThinWalls/Controllers/HomeController.cs
+++ ThinWalls/Controllers/HomeController.cs
@@ -13,8 +13,6 @@ namespace ThinWalls.Controllers
 {
     public class HomeController : Controller
     {
-        private ThinWallsEntities db = new ThinWallsEntities();
-
         public ActionResult Index()
         {
             return View();
@@ -23,7 +21,7 @@ namespace ThinWalls.Controllers
         public ActionResult Search(string name = "Grand Circus", string location = "1570+WOODWARD+AVE,+Detroit,+MI,", int zipcode = 48226, string category = "apartments,restaurants,cafes,coffee,desserts,hotelstravel,education,vocational,localflavor,nightlife", int radius = 10000)
 
         {
-            
+
             HttpWebRequest WR = WebRequest.CreateHttp($"https://api.yelp.com/v3/businesses/search?term={name}&location={location},{zipcode}&categories={category}&radius={radius}&sort_by=distance&limit=50");
             WR.UserAgent = "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0";
             string key = ConfigurationManager.AppSettings["AuthKey"];
@@ -56,6 +54,31 @@ namespace ThinWalls.Controllers
                 JObject JsonData = JObject.Parse(Data);
                 ViewBag.Data = JsonData["businesses"];
 
+                //Logic for displaying score on results page
+                ThinWallsEntities db = new ThinWallsEntities(); //pulls info from db
+                int average = 0;
+                List<Review> revlist = new List<Review>();
+                
+                for (int i = 0; i < JsonData["businesses"].Count(); i++)
+                {
+                    var id = JsonData["businesses"][i]["id"];
+                    revlist = (from r in db.Reviews
+                               where r.YelpID == (string)(id)
+                               select r).ToList();
+                }
+                foreach (var r in revlist)
+                {
+                    average += r.WallScore;
+                }
+                if (average == 0)
+                {
+                    ViewBag.Score = "No reviews yet. Be the first!";
+                }
+                else
+                {
+                    average = average / revlist.Count; //calculates average of all the scores
+                    ViewBag.Score = average; //so view can access the info            
+                }
             }
             catch (Exception e)
             {
@@ -68,14 +91,9 @@ namespace ThinWalls.Controllers
 
         }
 
+        
         public ActionResult Details(string id)
         {
-            List<Review> reviews = (from r in db.Reviews
-                                    where r.YelpID == id
-                                    select r).ToList();
-
-            ViewBag.Reviews = reviews;
-
             HttpWebRequest WR = WebRequest.CreateHttp($"https://api.yelp.com/v3/businesses/{id}");
             WR.UserAgent = "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0";
             string key = ConfigurationManager.AppSettings["AuthKey"];
@@ -105,7 +123,7 @@ namespace ThinWalls.Controllers
 
             try
             {
-                JObject JsonData = JObject.Parse(Data);
+                JObject JsonData = JObject.Parse(Data);               
                 ViewBag.Data = JsonData;
 
             }
@@ -115,12 +133,32 @@ namespace ThinWalls.Controllers
                 ViewBag.ErrorDescription = e.Message;
                 return View();
             }
-                        
 
+            //Calculate Wall Score Logic
+            ThinWallsEntities db = new ThinWallsEntities(); //pulls info from db
+            List<Review> reviews = (from r in db.Reviews
+                                 where r.YelpID == id
+                                 select r).ToList(); //pulls only item w/ matching yelpid
+            ViewBag.Reviews = reviews;
+            int average = 0;
+            foreach(var r in reviews)
+            {
+                average += r.WallScore;
+            }            
+            if (average == 0)
+            {
+                ViewBag.Score = "No reviews yet. Be the first!";
+            }
+            else
+            {
+                average = average / reviews.Count; //calculates average of all the scores
+                ViewBag.Score = average; //so view can access the info            
+            }
+            
             return View();
         }
 
-
+               
 
 
     }
